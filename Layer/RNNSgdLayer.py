b0664d4fe911fd0e77fc8074c428ee4d2736f171
modkzs <746061261@qq.com>
2016-01-27 11:19:58 +0800

    add queue in RnnLayer to implement bptt

--- Layer/RNNSgdLayer.py
+++ Layer/RNNSgdLayer.py
@@ -5,7 +5,10 @@ from Function import function
 __author__ = 'yixuanhe'
 
 class RNNSgdLayer(PlainLayer.PlainLayer):
-    """RNN layer in neural network using sgd"""
+    """RNN layer in neural network using sgd.
+    This layer can only be used as hidden layer
+    because only hidden layer need to get the
+    output value of last layer as input"""
 
     def __init__(self, cell_num, func, input_num, backoff):
 
@@ -24,12 +27,25 @@ class RNNSgdLayer(PlainLayer.PlainLayer):
 
         self.backoff = backoff
 
-        pass
+        self.s = []
+        self.x = []
+        for i in range(backoff):
+            self.s.append(0)
+            self.x.append(0)
+
+    def _push_value(self, l, value):
+        """This is a pop operation of queue implemented by list"""
+        l.pop(1)
+        l.append(value)
 
     def computeNet(self, input, layer):
         input = np.append(input, [1])
+
         self.input = input
+        self._push_value(self.x, input)
         self.layer = layer
+        self._push_value(self.s, layer)
+
         self.net = np.dot(self.input_weight, input)
         self.net += np.dot(self.layer_weight, layer)
 
@@ -41,7 +57,6 @@ class RNNSgdLayer(PlainLayer.PlainLayer):
 
         for n in self.net:
             value.append(self.func.f(n))
-
         return np.array(value)
 
     def getDerivative(self, err, x):
@@ -50,7 +65,7 @@ class RNNSgdLayer(PlainLayer.PlainLayer):
 
 if __name__ == '__main__':
     sg = function.Sigmoid()
-    layer = RNNSgdLayer(2, sg, 5)
+    layer = RNNSgdLayer(2, sg, 5, 4)
 
     x = np.random.normal(loc=0.0, scale=1.0, size=5)
     l = np.random.normal(loc=0.0, scale=1.0, size=2)
@@ -58,3 +73,8 @@ if __name__ == '__main__':
 
     layer.computeNet(x, l)
     layer.computeOutput()
+
+    a = [1, 2, 3, 4, 5, 6]
+    a.pop(0)
+    a.append(7)
+    print(a)
